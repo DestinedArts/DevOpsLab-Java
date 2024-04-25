c98f71681351ca55514c9f196cc9d776989025b3
DestinedArts <minalee329761@gmail.com>
2024-04-24 22:55:15 -0700

    NameChecker method

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -11,8 +11,36 @@ public class NameChecker {
      * @return True if input is a valid name, else false
      */
     public static boolean check(String input) {
-        // TODO: implement
+        if (input.length() < 2 || input.length() > 40)
+            return false;
 
-        return false;
+        // check first character is letter
+        char ch = input.charAt(0);
+        if (!Character.isLetter(ch))
+            return false;
+        
+        char prevch = ch;
+        boolean hasQuote = false;
+
+        for (int i = 1; i < input.length(); i++)
+        {
+            ch = input.charAt(i);
+            // check for consecutive hyphen
+            if (ch == '-' && prevch == '-')
+                return false;
+            // check for single quote
+            if (ch == '\'')
+            {
+                if (hasQuote)
+                    return false;
+                hasQuote = true;
+            }
+            // check for invalid characters
+            if (!Character.isLetter(ch) && ch != '\'' && ch != '-')
+                return false;
+            prevch = ch;
+        }
+            
+        return true;
     }
 }
